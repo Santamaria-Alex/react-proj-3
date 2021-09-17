4f95ffb5a385bd9223194486196a3ad1d3cb3921
Alex Santamaria <alexsantamaria@Alexs-MacBook-Pro.local>
2021-09-16 19:27:08 -0500

    working on adding input values into lists

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -11,16 +11,21 @@ const AddUser = props => {
     //in this case, preventDefault prevents the page from refreshing when SUBMIT button is clicked. 
     const addUserHandler = (event) => {
         event.preventDefault();
+
+        //prevent submit if fields are empty
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
             return; //return statements finish the function execution
         }
 
         //adding a '+' turns enteredAge, which is a string, to a number
+        //prevent submit if age is <1
         if (+enteredAge < 1) {
             return;
         }
 
         console.log(enteredUsername, enteredAge);
+
+        //set input fields to blank once submitted
         setEnteredAge('');
         setEnteredUsername('');
     }
@@ -40,6 +45,7 @@ const AddUser = props => {
                 <input
                     type="text"
                     id="username"
+                    //set value to reset input fields
                     value={enteredUsername}
                     onChange={usernameChangeHandler} />
 
@@ -47,6 +53,7 @@ const AddUser = props => {
                 <input
                     id="age"
                     type="number"
+                    //set value to reset input fields
                     value={enteredAge}
                     onChange={ageChangeHandler} />
 
