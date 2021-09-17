e9a60f1de8f1d40657804c732ce8b92dc4b86ab2
Alex Santamaria <alexsantamaria@Alexs-MacBook-Pro.local>
2021-09-17 17:27:28 -0500

    made error modal responsive, project complete

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,71 +1,91 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css';
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = props => {
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+  //in this case, preventDefault prevents the page from refreshing when SUBMIT button is clicked.
+  const addUserHandler = (event) => {
+    event.preventDefault();
 
-    //in this case, preventDefault prevents the page from refreshing when SUBMIT button is clicked. 
-    const addUserHandler = (event) => {
-        event.preventDefault();
-
-        //prevent submit if fields are empty
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-            return; //return statements finish the function execution
-        }
-
-        //adding a '+' turns enteredAge, which is a string, to a number
-        //prevent submit if age is <1
-        if (+enteredAge < 1) {
-            return;
-        }
-
-        //calling this up on App.js
-        props.onAddUser(enteredUsername, enteredAge);
-
-        //set input fields to blank once submitted
-        setEnteredAge('');
-        setEnteredUsername('');
+    //prevent submit if fields are empty
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age.",
+      });
+      return; //return statements finish the function execution
     }
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
+    //adding a '+' turns enteredAge, which is a string, to a number
+    //prevent submit if age is <1
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age.",
+      });
+      return;
     }
 
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    }
+    //calling this up on App.js
+    props.onAddUser(enteredUsername, enteredAge);
 
-    return (
-        <div>
-            <ErrorModal title='An error has occured!' message='Something went wrong.'/>
-            <Card className={classes.input}>
-                <form onSubmit={addUserHandler}>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        id="username"
-                        //set value to reset input fields
-                        value={enteredUsername}
-                        onChange={usernameChangeHandler} />
+    //set input fields to blank once submitted
+    setEnteredAge("");
+    setEnteredUsername("");
+  };
 
-                    <label htmlFor="age">Age (Years)</label>
-                    <input
-                        id="age"
-                        type="number"
-                        //set value to reset input fields
-                        value={enteredAge}
-                        onChange={ageChangeHandler} />
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
 
-                    <Button type="submit">Add User</Button>
-                </form>
-            </Card>
-        </div>
-    )
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            //set value to reset input fields
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
+
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            //set value to reset input fields
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default AddUser;
